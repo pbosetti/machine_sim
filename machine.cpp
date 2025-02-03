5f123d6eadc421d6d0b95a1944c32d13e96c62bb
Paolo Bosetti <paolo.bosetti@unitn.it>
2025-02-03 13:36:14 +0100

    WIP

--- machine.cpp
+++ machine.cpp
@@ -4,7 +4,9 @@
 #include <QList>
 #include <algorithm>
 #include <math.h>
-#include <yaml-cpp/yaml.h>
+// #include <yaml/yaml.h>
+#include <fstream>
+#include <fkYAML/node.hpp>
 
 
 QDebug operator<<(QDebug dbg, Machine *m) {
@@ -83,28 +85,31 @@ void Machine::loadIniFile(QString &path) {
       _axes[axis]->d = config[ax]["d"].value_or(0.0);
     }
   } else if (path.endsWith(".yml")) {
-    auto config = YAML::LoadFile(path.toStdString());
-    _brokerAddress = QString::fromStdString(config["mqtt"]["host"].as<std::string>("localhost"));
-    _brokerPort = config["mqtt"]["port"].as<int>(1883);
-    _subTopic = QString::fromStdString(config["mqtt"]["topics"]["pub"].as<std::string>("cnc/setpoint"));
-    _pubTopic = QString::fromStdString(config["mqtt"]["topics"]["sub"].as<std::string>("cnc/status/#"));
-    _tq = config["machine"]["tq"].as<double>(0.005);
+      std::ifstream ifs(path.toStdString());
+      auto config = fkyaml::node::deserialize(ifs);
+      _brokerAddress = QString::fromStdString(config["mqtt"]["host"].get_value<std::string>());
+    // auto config = YAML::LoadFile(path.toStdString());
+    // _brokerAddress = QString::fromStdString(config["mqtt"]["host"].as<std::string>("localhost"));
+    // _brokerPort = config["mqtt"]["port"].as<int>(1883);
+    // _subTopic = QString::fromStdString(config["mqtt"]["topics"]["pub"].as<std::string>("cnc/setpoint"));
+    // _pubTopic = QString::fromStdString(config["mqtt"]["topics"]["sub"].as<std::string>("cnc/status/#"));
+    // _tq = config["machine"]["tq"].as<double>(0.005);
 
-    for (auto &axis: _axesTags) {
-      auto ax = _axesNames[axis];
-      _axes[axis]->length = config["machine"]["axes"][ax]["length"].as<double>(1.0);
-      _axes[axis]->friction = config["machine"]["axes"][ax]["friction"].as<double>(1000.0);
-      _axes[axis]->mass = config["machine"]["axes"][ax]["mass"].as<double>(150.0);
-      _axes[axis]->effective_mass = _axes[axis]->mass;
-      _axes[axis]->max_torque = config["machine"]["axes"][ax]["max_torque"].as<double>(20.0);
-      _axes[axis]->pitch = config["machine"]["axes"][ax]["pitch"].as<double>(0.01);
-      _axes[axis]->gravity = config["machine"]["axes"][ax]["gravity"].as<double>(0.0);
-      _axes[axis]->integration_dt =
-          config["machine"]["axes"][ax]["integration_dt"].as<double>(5);
-      _axes[axis]->p = config["machine"]["axes"][ax]["p"].as<double>(1.0);
-      _axes[axis]->i = config["machine"]["axes"][ax]["i"].as<double>(0.0);
-      _axes[axis]->d = config["machine"]["axes"][ax]["d"].as<double>(0.0);
-    }
+    // for (auto &axis: _axesTags) {
+    //   auto ax = _axesNames[axis];
+    //   _axes[axis]->length = config["machine"]["axes"][ax]["length"].as<double>(1.0);
+    //   _axes[axis]->friction = config["machine"]["axes"][ax]["friction"].as<double>(1000.0);
+    //   _axes[axis]->mass = config["machine"]["axes"][ax]["mass"].as<double>(150.0);
+    //   _axes[axis]->effective_mass = _axes[axis]->mass;
+    //   _axes[axis]->max_torque = config["machine"]["axes"][ax]["max_torque"].as<double>(20.0);
+    //   _axes[axis]->pitch = config["machine"]["axes"][ax]["pitch"].as<double>(0.01);
+    //   _axes[axis]->gravity = config["machine"]["axes"][ax]["gravity"].as<double>(0.0);
+    //   _axes[axis]->integration_dt =
+    //       config["machine"]["axes"][ax]["integration_dt"].as<double>(5);
+    //   _axes[axis]->p = config["machine"]["axes"][ax]["p"].as<double>(1.0);
+    //   _axes[axis]->i = config["machine"]["axes"][ax]["i"].as<double>(0.0);
+    //   _axes[axis]->d = config["machine"]["axes"][ax]["d"].as<double>(0.0);
+    // }
   } else {
     return;
   }
